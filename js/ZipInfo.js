e1f29b5ea3734927096b308dacc06b2a4dbe47db
tokushima <tokushima@10.0.1.1>
2017-08-22 18:26:02 +0900

    事業所も取得する

--- js/ZipInfo.js
+++ js/ZipInfo.js
@@ -1,29 +1,28 @@
 var ZipInfo = {};
 
-ZipInfo.set = function(base_url,frm_zip,frm_prefecture,frm_address1,frm_address2){
-console.log(frm_zip.value);	
+ZipInfo.set = function(base_url,frm_zip,frm_prefecture,frm_city,frm_area){
 	zip1 = frm_zip.value.slice(0,3);
 	zip2 = frm_zip.value.slice(3,5);
 	zip3 = frm_zip.value.slice(5,7);
 
 	$.ajax({
 		type: 'GET',
-		url: base_url + "/" + zip1 + "/" + zip2 + ".json",
+		url: base_url + '/' + zip1 + '/' + zip2 + '.json',
 		dataType: 'json',
 		cache: false,
 		success: function(json){
-			prefecture = json["prefecture"];
-			addr = json["addr"];
-			address2 = addr[zip3];
+			prefecture = json['prefecture'];
+			addr = json['addr'];
+			area = (zip3 in addr == true) ? addr[zip3] : '';
 			
-			if($.isArray(address2)){
-				frm_prefecture.value = address2[0];
-				frm_address1.value = address2[1];
-				frm_address2.value = address2[2];
+			if($.isArray(area)){
+				frm_prefecture.value = area[0];
+				frm_city.value = area[1];
+				frm_area.value = area[2];
 			}else{
 				frm_prefecture.value = prefecture[0];
-				frm_address1.value = prefecture[1];
-				frm_address2.value = address2;
+				frm_city.value = prefecture[1];
+				frm_area.value = area;
 			}
 		},
 		error:function(data){
--- lib/ZipInfo/cmd/publish.php
+++ lib/ZipInfo/cmd/publish.php
@@ -6,93 +6,114 @@
  */
 $out_dir = $out;
 $work_dir = $work;
-$ken_all_url = 'http://www.post.japanpost.jp/zipcode/dl/roman/ken_all_rome.zip';
+$ken_all_url = 'http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip';
+$jigyosyo_url = 'http://www.post.japanpost.jp/zipcode/dl/jigyosyo/zip/jigyosyo.zip';
 
 if(empty($work)){
 	if(class_exists('\ebi\WorkingStorage')){
-		$in_dir = \ebi\WorkingStorage::path('ZipInfo');
+		$work_dir = \ebi\WorkingStorage::path('ZipInfo');
 	}else{
-		$in_dir = getcwd().'/work/ZipInfo';
+		$work_dir = getcwd().'/work/ZipInfo';
 	}
 }
 
 ini_set('memory_limit','-1');
 
-if(!is_dir($in_dir)){
-	mkdir($in_dir,0777,true);
+if(!is_dir($work_dir)){
+	mkdir($work_dir,0777,true);
 }
-file_put_contents($in_dir.'/ken_all_rome.zip',file_get_contents($ken_all_url));
-
-$zip = new \ZipArchive();
-$zip->open($in_dir.'/ken_all_rome.zip');
-$zip->extractTo($in_dir);
-$zip->close();
-unlink($in_dir.'/ken_all_rome.zip');
-
-$src = mb_convert_encoding(file_get_contents($in_dir.'/KEN_ALL_ROME.CSV'),'UTF-8','SJIS');
-$src = mb_convert_kana($src,'as');
-$src = str_replace(['（','）'],['(',')'],$src);
-$src = str_replace(' ','',$src);
-$src = str_replace('"','',$src);
-unlink($in_dir.'/KEN_ALL_ROME.CSV');
-rmdir($in_dir);
-
 if(!is_dir($out_dir)){
 	mkdir($out_dir,0777,true);
 }
+
+$download_func = function($url,$csv_filename) use($work_dir){	
+	$basename = basename($url);
+	file_put_contents($work_dir.'/'.$basename,file_get_contents($url));
+	
+	$zip = new \ZipArchive();
+	$zip->open($work_dir.'/'.$basename);
+	$zip->extractTo($work_dir);
+	$zip->close();
+	
+	unlink($work_dir.'/'.$basename);
+	
+	$src = mb_convert_encoding(file_get_contents($work_dir.'/'.$csv_filename),'UTF-8','SJIS');
+	$src = str_replace(' ',' ',$src);
+	$src = str_replace('"','',$src);
+	unlink($work_dir.'/'.$csv_filename);	
+	return $src;
+};
+$parse_ken_all_func = function($line){
+	list(,,$zip,,,,$pref,$city,$area) = explode(',',$line);
+	return [$zip,$pref,$city,$area];
+};
+$parse_jigyosyo_func = function($line){
+	list(,,$com,$pref,$city,$area1,$area2,$zip) = explode(',',$line);
+	return [$zip,$pref,$city,$area1.$area2.'　'.$com];
+};
+
+
 
 $overwrite_zip = [];
 $addr = [];
 $cnt = 0;
 $zipcnt = 0;
 
-foreach(explode(PHP_EOL,$src) as $line){
-	if(!empty($line)){
-		list($zip,$state,$address1,$address2) = explode(',',$line);
-		$cnt++;
-
-		$zip1 = substr($zip,0,3);
-		$zip2 = substr($zip,3,2);
-		$zip3 = substr($zip,5);
-		
-		$address1 = str_replace(' ','',$address1);
-		
-		$address2 = preg_replace('/\(.+\)/','', $address2);
-		$address2 = preg_replace('/\(.*$/','',$address2);
-		$address2 = preg_replace('/^.*\).*$/','',$address2);
-
-		if($address2 == '以下に掲載がない場合'){
-			$address2 = '';
-		}else if(strpos($address2,'次に番地がくる場合') !== false){
-			$address2 = '';
-		}else if(strpos($address2,'一円') !== false){
-			$address2 = str_replace('一円','',$address2);
-		}else if(strpos($address2,'、') !== false){
-			$address2 = '';
-		}
-		
-		if(!isset($addr[$zip1][$zip2]['prefecture'])){
-			$addr[$zip1][$zip2]['prefecture'] = [$state,$address1];
-		}		
-		if(implode('',$addr[$zip1][$zip2]['prefecture']) != $state.$address1){
-			$address2 = [$state,$address1,$address2];
-		}
-		
-		if(isset($addr[$zip1][$zip2]['addr'][$zip3])){
-			if($addr[$zip1][$zip2]['prefecture'] != [$state,$address1] || $addr[$zip1][$zip2]['addr'][$zip3] != $address2){
-				$overwrite_zip[] = array_merge(
-					[$zip],
-					(is_array($addr[$zip1][$zip2]['addr'][$zip3]) ?
-						$addr[$zip1][$zip2]['addr'][$zip3] :
-						array_merge($addr[$zip1][$zip2]['prefecture'],[$addr[$zip1][$zip2]['addr'][$zip3]])
-					)
-				);
-				$zipcnt--;
+foreach([
+	$ken_all_url=>[$parse_ken_all_func,'KEN_ALL.CSV'],
+	$jigyosyo_url=>[$parse_jigyosyo_func,'JIGYOSYO.CSV']
+] as $url => $datainfo){
+	
+	foreach(explode(PHP_EOL,$download_func($url,$datainfo[1])) as $line){
+		if(!empty($line)){
+			list($zip,$pref,$city,$area) = $datainfo[0]($line);
+			$cnt++;
+	
+			$zip1 = substr($zip,0,3);
+			$zip2 = substr($zip,3,2);
+			$zip3 = substr($zip,5);
+			
+			$city = str_replace(' ','',$city);
+			
+			
+			if(preg_match('/\（.+$/',$area,$m)){
+				if(mb_substr($m[0],-2) !== '階）'){
+					$area = str_replace($m[0],'',$area);
+				}
 			}
+			if(
+				mb_strpos($area,'以下に掲載がない場合') !== false ||
+				mb_strpos($area,'次に番地がくる場合') !== false ||
+				mb_strpos($area,'一円') !== false ||
+				mb_strpos($area,'、') !== false ||
+				mb_strpos($area,'〜') !== false
+			){
+				$area = '';
+			}
+			
+			if(!isset($addr[$zip1][$zip2]['prefecture'])){
+				$addr[$zip1][$zip2]['prefecture'] = [$pref,$city];
+			}		
+			if(implode('',$addr[$zip1][$zip2]['prefecture']) != $pref.$city){
+				$area = [$pref,$city,$area];
+			}
+			
+			if(isset($addr[$zip1][$zip2]['addr'][$zip3])){
+				if($addr[$zip1][$zip2]['prefecture'] != [$pref,$city] || $addr[$zip1][$zip2]['addr'][$zip3] != $area){
+					$overwrite_zip[] = array_merge(
+						[$zip],
+						(is_array($addr[$zip1][$zip2]['addr'][$zip3]) ?
+							$addr[$zip1][$zip2]['addr'][$zip3] :
+							array_merge($addr[$zip1][$zip2]['prefecture'],[$addr[$zip1][$zip2]['addr'][$zip3]])
+						)
+					);
+					$zipcnt--;
+				}
+			}
+			
+			$addr[$zip1][$zip2]['addr'][$zip3] = $area;
+			$zipcnt++;
 		}
-		
-		$addr[$zip1][$zip2]['addr'][$zip3] = $address2;
-		$zipcnt++;
 	}
 }
 
